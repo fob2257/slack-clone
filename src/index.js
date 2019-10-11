6a66a5b78ce4c5ae8d19195afaeff8cc96ab5c29
Saul Meneses <fob2257@gmail.com>
2019-10-11 12:10:40 -0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-css/semantic.min.css';
+
+import HomePage from './components/App';
+import Register from './components/Register';
+import LogIn from './components/LogIn';
+
+const Root = () => (
+  <div className='root'>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/register' component={Register} />
+        <Route path='/logIn' component={LogIn} />
+      </Switch>
+    </Router>
+  </div>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
