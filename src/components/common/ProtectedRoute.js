4a685dd23853afe7acebf8c150d89d3299604624
Saul M <fob2257@gmail.com>
2019-10-14 17:43:56 -0700

    Protected route component

---
+++ src/components/common/ProtectedRoute.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const ProtectedRoute = ({ component: Component, currentUser, ...rest }) => (
+  <Route
+    render={props => (!currentUser) ? <Redirect to='/logIn' />
+      : <Component {...props} />}
+    {...rest}
+  />
+);
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
+export default connect(mapStateToProps)(ProtectedRoute);
